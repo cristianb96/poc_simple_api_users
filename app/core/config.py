59b0ed552344713458dda8cb8f4d2b16680f7673
cristianb96 <55033854+cristianb96@users.noreply.github.com>
2025-08-18 21:13:45 -0500

    non

--- app/core/config.py
+++ app/core/config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "tu_clave_secreta_muy_segura_aqui_cambiala_en_produccion")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN", "30"))
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./users.db")
 
 settings = Settings()
